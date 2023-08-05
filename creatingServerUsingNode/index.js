54822a9f164138dcd388ddf03be5d371e78a86a7
Jagroshan <jagroshansingh615@gmail.com>
2023-08-05 16:29:10 +0530

    accessed IPv4 address along with IPv6

--- creatingServerUsingNode/index.js
+++ creatingServerUsingNode/index.js
@@ -2,21 +2,23 @@ const http=require('http')
 
 const server = http.createServer((req, res) => {
     // Get the IP address of the client from the request object
-    const ipAddress = req.connection.remoteAddress
+    const ipAddress = req.socket.remoteAddress
     console.log(`Client IP address: ${ipAddress}`);
   
     res.end('Hello!');
   });
   
 
-server.listen(8200,()=>{
+server.listen(8200,'0.0.0.0',()=>{
     console.log('Server is running at port 8200')
 })
 
-//-------------Let me try with express
+//-------------Let me try the same with express------------------
 
-// const express=require('express')
-// const app=express()
+const express=require('express')
+const app=express()
+
+// app.set('trust proxy',true);       //when proxy is in place from client side
 
 // app.use((req,res,next)=>{
 //     const ipAdress=req.ip;
@@ -25,6 +27,21 @@ server.listen(8200,()=>{
 //     next();
 // })
 
-// app.listen(8300,()=>{
+//----------------for extracting IPV4 address------------------
+// app.use((req, res, next) => {
+//     // console.log(req.ip)
+//     const ipv4=req.socket.remoteAddress
+//     req.clientIp = ipv4;
+//     next();
+//   });
+  
+//   // Route to test
+//   app.get('/', (req, res) => {
+//     console.log(`Client IP address: ${req.clientIp}`);
+//     res.send('Hello, World!');
+//   });
+
+// //By passing '0.0.0.0' as the second argument, the server starts listening to both IPv4 and IPv6
+// app.listen(8300,'0.0.0.0',()=>{
 //     console.log('Server is running at 8300')
 // })
