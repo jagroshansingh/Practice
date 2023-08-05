111ae95eece7ba22617850d343113bb34724dd90
Jagroshan <jagroshansingh615@gmail.com>
2023-08-05 16:54:57 +0530

    revising Date thing

---
+++ implementTimeStamp/index.js
@@ -0,0 +1,6 @@
+// console.log(new Date())
+let date=new Date()
+console.log(date.getDate()+"/"+date.getMonth()+"/"+date.getFullYear())
+console.log(date.getHours()+":"+date.getMinutes())
+
+console.log(Date())
